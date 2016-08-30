e591fc71eb271209a594d2ccefdba920551dca5b
Michael <emporophobe@gmail.com>
2016-08-29 22:31:46 -0400

    Refactored Game class to separate UI from the game logic. new_round now takes a bunch of lambdas to customize it, mostly so I could see if it worked. Also fixed how topics were selected so they won't appear multiple times per game any more.

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
+from __future__ import print_function
 from Censor import *
-import random
 
 
 class Game:
@@ -45,44 +45,65 @@ class Game:
         if self._guesses < 1:
             self._guesses = 1
 
-    def new_round(self):
+    def new_round(self, topic, display_headline, prompt_for_guess, on_correct_guess, on_bad_guess, on_loss):
         """
         Play a single round of the game.
 
-        Generate and print headlines based on one of the topics in the topics file
-        and display censored versions of the headlines. Prompt user for their guess of what the search term was.
+        This function takes a number of function arguments in order to easily adapt to different user interfaces
+        e.g. passing print functions in to play in console, or passing in tkinter-based calls for a basic game GUI
+
+        :param topic: The search query/answer
+        :param display_headline: (Str -> None) Display a single headline string
+        :param prompt_for_guess: (None -> Str) Prompt the user for a guess and passes it to the function as a string
+        :param on_correct_guess: (None -> None) What to do when the user guesses correctly
+        :param on_bad_guess: (None -> None) What to do when the user guesses incorrectly
+        :param on_loss: (None -> None) What to do when the user runs out of guesses
         :return: Whether the user's guess was correct.
         """
-        topic = random.choice(self._topic_list)
         headlines = self._censor.create_censored_headlines(topic, self._random_censoring_proportion)
 
         for headline in random.sample(headlines, max(self._num_headlines, len(headlines))):
-            print headline
+            display_headline(headline)
         won = False
         for i in xrange(self._guesses):
-            prompt = "> " if len(topic.split()) == 1 else "{0} words > ".format(len(topic.split()))
-            guess = raw_input(prompt)
+            guess = prompt_for_guess()
             if guess.lower() == topic.lower():
-                print "Yay!"
+                on_correct_guess()
                 won = True
                 break
             else:
-                print ":("
+                on_bad_guess()
                 continue
         if not won:
-            print "Answer: {0}".format(topic)
+            on_loss()
         return won
 
-    def play(self, rounds):
+    def new_terminal_round(self, topic):
         """
-        Play a set number of rounds.
+        Create a version of new_round that interacts with the user via terminal
+        :param topic: The search query/answer
+        :return: Whether the user guessed correctly
+        """
+        return self.new_round(
+            topic,
+            print,
+            lambda: raw_input("> " if len(topic.split()) == 1 else "{0} words > ".format(len(topic.split()))),
+            lambda: print("yay"),
+            lambda: print("boo"),
+            lambda: print("Answer: {0}".format(topic))
+        )
 
-        :param rounds: The number of rounds
-        :return: The number of rounds the user won (i.e. guessed the search term correctly)
+    def play_in_terminal(self, rounds):
         """
+        Play a set number of rounds in the terminal
+        :param rounds: The number of rounds to play
+        :return: The number of rounds won
+        """
+        # sample topics without replacement so we don't see the same headlines twice in a game
+        topics = random.sample(self._topic_list, min(len(self._topic_list), rounds))
         won = 0
-        for i in xrange(rounds):
-            if self.new_round():
+        for topic in topics:
+            if self.new_terminal_round(topic):
                 won += 1
         return won
 
@@ -96,8 +117,8 @@ def main():
     """
     g = Game("topics.txt")
     rounds = 5
-    won = g.play(rounds)
-    print "{0}/{1} correct".format(won, rounds)
+    won = g.play_in_terminal(rounds)
+    print("{0}/{1} correct".format(won, rounds))
     raw_input()  # Keep from exiting immediately
 
 if __name__ == "__main__":
